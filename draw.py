9f07e2bd3f7621d3a5b2a1b4a9ccfb3f5c90d20a
randheer <randheer.dakshana15@gmail.com>
2021-03-21 23:04:24 +0530

    somenew

--- draw.py
+++ draw.py
@@ -2,14 +2,13 @@ import cv2
 import numpy as np
 
 img = np.zeros([1000, 1000, 3],dtype='uint8' )
-cv2.line(img, (0,0,), (475,475), (255,0,0), thickness=2)
-cv2.line(img, (475,475), (850,0), (255,0,0),thickness=2)
+cv2.line(img, (475,475), (850,0), (255,255,255),thickness=2)
 
 cv2.rectangle(img, (000,000), (500, 1000), (0,255,0), thickness=cv2.FILLED)
 
-
 font=cv2.FONT_HERSHEY_SIMPLEX
-cv2.putText(img, "hello randheer", (10,800), font, 1, (0,0,255), 2, cv2.LINE_AA)
+cv2.putText(img, "Hello Randheer", (600,500), font, 1, (0,0,255), 2, cv2.LINE_AA)
+cv2.circle(img, (img.shape[1]//2,img.shape[0]//2),40, (0,0,255), thickness=-1)
 
 cv2.imshow('original', img)
 cv2.waitKey(10000)
